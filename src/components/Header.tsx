3f2e8914b10c21cfff8e773ca52484c3b89c6532
Rasul <jolyskytner@gmail.com>
2022-11-29 15:50:51 +0500

    test

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,28 @@
-import React from 'react'
 import logo from "../assets/logo.svg";
 const Header = () => {
   return (
     <header className="header">
-        <div className="header__wrapper">
-          <div className="header__logo">
-            <img src={logo} alt="" />
-          </div>
-          <div className="header__nav">
-            <ul>
-              <li>Мониторинг</li>
-              <li>Обменники</li>
-              <li>Партнёрам</li>
-              <li>Новости</li>
-            </ul>
-          </div>
-          <div className="header__contacts">
-            <ul>
-              <li>Поддержка</li>
-              <li>Контакты</li>
-            </ul>
-          </div>
+      <div className="header__wrapper">
+        <div className="header__logo">
+          <img src={logo} alt="" />
         </div>
-      </header>
-  )
-}
+        <div className="header__nav">
+          <ul>
+            <li>Мониторинг</li>
+            <li>Обменники</li>
+            <li>Партнёрам</li>
+            <li>Новости</li>
+          </ul>
+        </div>
+        <div className="header__contacts">
+          <ul>
+            <li>Поддержка</li>
+            <li>Контакты</li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
