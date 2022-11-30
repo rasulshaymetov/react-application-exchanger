172df932e3ccd470ab22b25f4366e9593684fd86
Rasul <jolyskytner@gmail.com>
2022-11-30 13:42:07 +0500

    added data

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,68 @@ import { ICards } from "./models/models";
 import logo from "./assets/card-logo.svg";
 const CARDS: any = [
   {
-    id: "1",
-    title: "Тинькофф",
-    currency: ["RUB", "USD", "EUR"],
+    heading: "Банки",
+    items: [
+      {
+        id: "1",
+        title: "Тинькофф",
+        rating: "4",
+        currency: ["RUB", "USD", "EUR"],
+      },
+      {
+        id: "2",
+        title: "Альфа Банк",
+        rating: "4",
+        currency: ["RUB", "USD"],
+      },
+      {
+        id: "3",
+        title: "Русский Стандарт",
+        rating: "4",
+
+        currency: ["RUB"],
+      },
+      { id: "4", title: "Открытие", rating: "4", currency: ["RUB", "USD"] },
+    ],
   },
   {
-    id: "2",
-    title: "Альфа Банк",
-    currency: ["RUB", "USD"],
+    heading: "Переводы",
+    items: [
+      {
+        id: "1",
+        title: "Открытие",
+        rating: "4",
+
+        currency: ["RUB", "USD"],
+      },
+    ],
   },
   {
-    id: "3",
-    title: "Русский Стандарт",
-    currency: ["RUB"],
+    heading: "Криптовалюты",
+    items: [
+      {
+        id: "1",
+        title: "Bitcoin",
+        rating: "4",
+
+        currency: ["BTC", "WBTC", "BTCBEP20"],
+      },
+      {
+        id: "1",
+        title: "Etherium",
+        rating: "4",
+
+        currency: ["ETH", "ETHBEP20"],
+      },
+      {
+        id: "1",
+        title: "Litecoin",
+        rating: "4",
+
+        currency: ["LTC"],
+      },
+    ],
   },
-  { id: "4", title: "Открытие", currency: ["RUB", "USD"] },
 ];
 function App() {
   return (
@@ -34,34 +81,42 @@ function App() {
             <Search />
             <div className="cards">
               <div className="cards__wrapper">
-                {CARDS.map(function (card: any) {
+                {CARDS?.map(function (card: any) {
                   return (
-                    <div key={card.id} className="cards__component">
-                      {/* <h1>Банки</h1> */}
-                      <div className="cards__card">
-                        <div className="cards__card_wrapper">
-                          <div className="cards__top">
-                            <div className="cards__logo">
-                              <img src={logo} alt="" />
-                            </div>
-                            <p className="cards__title">{card.title}</p>
-                          </div>
-                          <div className="cards__currency">
-                            {card.currency.map(
-                              (type: string, index: number) => {
-                                return (
-                                  <div
-                                    className="cards__currency_value"
-                                    key={index}
-                                  >
-                                    {type}
+                    <div className="cards__block">
+                      <h1 onClick={test}>{card.heading}</h1>
+                      <div key={card.id} className="cards__component">
+                        {card.items?.map(function (item: any) {
+                          return (
+                            <div className="cards__card">
+                              <div className="cards__card_wrapper">
+                                <div className="cards__top">
+                                  <div className="cards__logo">
+                                    <img src={logo} alt="" />
                                   </div>
-                                );
-                              }
-                            )}
-                          </div>
-                        </div>
+                                  <p className="cards__title">{item.title}</p>
+                                </div>
+
+                                <div className="cards__currency">
+                                  {item.currency?.map(
+                                    (type: string, index: number) => {
+                                      return (
+                                        <div
+                                          className="cards__currency_value"
+                                          key={index}
+                                        >
+                                          {type}
+                                        </div>
+                                      );
+                                    }
+                                  )}
+                                </div>
+                              </div>
+                            </div>
+                          );
+                        })}
                       </div>
+                      <hr className="cards__line" />
                     </div>
                   );
                 })}
