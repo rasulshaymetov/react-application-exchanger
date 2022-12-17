dca5b1c612027a49bb31a906e42e842798529656
Rasul <jolyskytner@gmail.com>
2022-12-17 17:02:55 +0500

    added hide button layout

--- src/components/DirectionAside.tsx
+++ src/components/DirectionAside.tsx
@@ -1,30 +1,54 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import arrow from "../assets/sort.svg";
 const DirectionAside = () => {
   const { selectedCards } = useSelector((state: any) => state.array);
-  const [isClickedNav, setIsClickedNav] = useState('1')
+  const [isClickedNav, setIsClickedNav] = useState("1");
   return (
     <div className="aside">
       <div className="aside__wrapper">
         <div>
           <ul>
-            <div className={`${isClickedNav === '1' ? 'active-list' : null}`} onClick={() => setIsClickedNav("1")}>
-              <li>Тинькофф (USD) → <br/> Bitcoin (BTC) </li>
+            <div
+              className={`${isClickedNav === "1" ? "active-list" : null}`}
+              onClick={() => setIsClickedNav("1")}
+            >
+              <li>
+                Тинькофф (USD) → <br /> Bitcoin (BTC){" "}
+              </li>
             </div>
-            <div className={`${isClickedNav === '2' ? 'active-list' : null}`} onClick={() => setIsClickedNav("2")}>
-            <li>Тинькофф (USD) → <br/> Bitcoin (BTC) </li>
+            <div
+              className={`${isClickedNav === "2" ? "active-list" : null}`}
+              onClick={() => setIsClickedNav("2")}
+            >
+              <li>
+                Тинькофф (USD) → <br /> Bitcoin (BTC){" "}
+              </li>
             </div>
-            <div className={`${isClickedNav === '3' ? 'active-list' : null}`} onClick={() => setIsClickedNav("3")}>
-            <li>Тинькофф (USD) →<br/>  Bitcoin (BTC) </li>
+            <div
+              className={`${isClickedNav === "3" ? "active-list" : null}`}
+              onClick={() => setIsClickedNav("3")}
+            >
+              <li>
+                Тинькофф (USD) →<br /> Bitcoin (BTC){" "}
+              </li>
             </div>
-            <div className={`${isClickedNav === '4' ? 'active-list' : null}`} onClick={() => setIsClickedNav("4")}>
-            <li>Тинькофф (USD) → <br/> Bitcoin (BTC) </li>
+            <div
+              className={`${isClickedNav === "4" ? "active-list" : null}`}
+              onClick={() => setIsClickedNav("4")}
+            >
+              <li>
+                Тинькофф (USD) → <br /> Bitcoin (BTC){" "}
+              </li>
             </div>
             <li className="clear">Очистить историю поиска</li>
           </ul>
-
         </div>
+        <button className="hide-button">
+        <img src={arrow} alt="Hide button"/>
+      </button>
       </div>
+
     </div>
   );
 };
